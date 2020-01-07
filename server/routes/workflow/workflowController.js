df6329bb58082057e7b41f156852293aecd341bf
Ryan B <brockhoffrw@gmail.com>
2020-01-07 17:30:59 +0000

    feat: add workflow route logic.

--- server/routes/workflow/workflowController.js
+++ server/routes/workflow/workflowController.js
@@ -24,16 +24,52 @@ async function getWorkflows(req, res, next) {
 async function addWorkflow(req, res, next) {
   try {
     const workflow = snakeCaseKeys(req.body);
-    const [newWorkflow] = await knex("workflows")
+    const { wf_actions } = workflow;
+    delete workflow["wf_actions"];
+
+    //--Create New Workflow--//
+    const [newWfId] = await knex("workflows")
       .insert({
         ...workflow,
         uid: req.userId
       })
-      .returning("*");
+      .returning("wf_id");
+
+    //--Insert Workflow Actions--//
+    const snakeCaseWfActions = snakeCaseKeys(wf_actions).map(action => {
+      action["wf_id"] = newWfId;
+      action["uid"] = req.userId;
+      return action;
+    });
+    await knex("actions").insert(snakeCaseWfActions);
+
+    //--Retrieve Complete Workflow Object--//
+    const newWorkflow = await getIndividualWorkflow(req.userId, newWfId);
+    //--Send Back Workflow--//
     return res.status(200).json({ newWorkflow });
   } catch (err) {
     next(err);
   }
+}
+
+async function getIndividualWorkflow(userId, wfId) {
+  const {
+    rows: [workflow]
+  } = await knex.raw(`
+    with all_actions as (
+      select wf_id, array_agg(row_to_json(new_row)) as actions
+      from (
+         select wf_action_id, wf_id, wf_action_name, wf_action_type from actions
+         where uid = '${userId}'
+         and wf_id = '${wfId}'
+      ) new_row  
+      group by wf_id 
+      )
+      select * from workflows
+      right join all_actions on workflows.wf_id = all_actions.wf_id
+      limit 1;
+    `);
+  return workflow;
 }
 
 module.exports = { getWorkflows, addWorkflow };
