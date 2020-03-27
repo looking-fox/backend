4bd49e7839231aec13e675d1fd30a6c5d48186ea
Ryan B <brockhoffrw@gmail.com>
2020-03-27 15:17:50 -0700

    feat: initial addClient route logic.

--- server/routes/client/clientController.js
+++ server/routes/client/clientController.js
@@ -47,8 +47,40 @@ async function updateClientProgress(req, res, next) {
 
 async function addClient(req, res, next) {
   try {
-    await console.log("BODY: ", req.body);
-    return res.sendStatus(200);
+    let newClient = snakeCaseKeys(req.body);
+    newClient["uid"] = req.userId;
+    const [clientId] = await knex("clients")
+      .insert(newClient)
+      .returning("client_id");
+
+    const {
+      rows: [client]
+    } = await knex.raw(`
+    with steps as (
+    select new_row.wf_id, array_agg(row_to_json(new_row)) as wf_actions
+    from (
+        select actions.wf_id, wf_action_name, wf_action_type from actions
+        where uid = '${req.userId}'
+    ) new_row  
+    group by new_row.wf_id 
+    ),
+    
+    workflows as (select workflows.wf_id, wf_name, wf_tag_color, wf_actions from workflows
+    right join steps on steps.wf_id = workflows.wf_id
+    where uid = '${req.userId}'
+    and archived is false
+    order by workflows.updated_at desc)
+        
+    select client_id, client_full_name, client_email, client_phone, client_date,
+    client_private_note, client_archived, clients.created_at, clients.updated_at, 
+    current_wf_index, wf_name, wf_tag_color, wf_actions from clients
+    right join workflows on clients.wf_id = workflows.wf_id
+    where clients.uid = '${req.userId}'
+    and clients.client_id = ${clientId}
+    and client_archived is false
+    limit 1;
+    `);
+    return res.status(200).json({ client });
   } catch (err) {
     next(err);
   }
