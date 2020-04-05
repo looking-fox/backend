00d8731596fb6bf150a3ae32b6f029d809ba01e5
Ryan B <brockhoffrw@gmail.com>
2020-04-05 08:47:41 -0700

    refactor: include required bool in form field.

--- server/db/migrations/20200405084653_initFormFields.js
+++ server/db/migrations/20200405084653_initFormFields.js
@@ -1,17 +1,11 @@
 const { onUpdateTrigger } = require("../../../knexfile");
 
-exports.up = async function(knex, Promise) {
-  await knex.schema.createTable("form_fields", table => {
-    table
-      .increments("form_field_id")
-      .unsigned()
-      .primary();
+exports.up = async function (knex, Promise) {
+  await knex.schema.createTable("form_fields", (table) => {
+    table.increments("form_field_id").unsigned().primary();
 
     table.string("uid").notNullable();
-    table
-      .foreign("uid")
-      .references("uid")
-      .inTable("users");
+    table.foreign("uid").references("uid").inTable("users");
 
     table.integer("form_id").notNullable();
     table
@@ -22,6 +16,7 @@ exports.up = async function(knex, Promise) {
 
     table.text("form_field_title").notNullable();
     table.string("form_field_type").notNullable();
+    table.boolean("form_field_required").defaultTo(false).notNullable();
     table.text("form_field_description").nullable();
     table.text("form_field_placeholder").nullable();
     table.specificType("form_field_select_options", "text ARRAY").nullable();
@@ -29,18 +24,12 @@ exports.up = async function(knex, Promise) {
 
     table.integer("form_field_order").notNullable();
 
-    table
-      .timestamp("created_at")
-      .notNullable()
-      .defaultTo(knex.raw("now()"));
-    table
-      .timestamp("updated_at")
-      .notNullable()
-      .defaultTo(knex.raw("now()"));
+    table.timestamp("created_at").notNullable().defaultTo(knex.raw("now()"));
+    table.timestamp("updated_at").notNullable().defaultTo(knex.raw("now()"));
   });
   await knex.raw(onUpdateTrigger("form_fields"));
 };
 
-exports.down = async function(knex, Promise) {
+exports.down = async function (knex, Promise) {
   await knex.schema.dropTable("form_fields");
 };
