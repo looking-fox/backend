ba4ca8c8c2d532e79e4ed8a3ae2b00c82b92bc06
rwbrockhoff <brockhoffrw@gmail.com>
2020-04-13 13:31:37 -0700

    feat: new updateTask route.

--- server/routes/task/taskController.js
+++ server/routes/task/taskController.js
@@ -17,10 +17,22 @@ async function addTask(req, res, next) {
     const [newTask] = await knex("tasks")
       .insert({ uid: req.userId, task_column_id: columnId })
       .returning("*");
+    newTask.isNew = true;
     res.status(200).json({ newTask, columnId });
   } catch (err) {
     next(err);
   }
 }
 
-module.exports = { getTasks, addTask };
+async function updateTask(req, res, next) {
+  try {
+    const { task } = req.body;
+    console.log("Task: ", task);
+    await console.log("PARAMS: ", req.params);
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { getTasks, addTask, updateTask };
--- server/routes/task/taskRoutes.js
+++ server/routes/task/taskRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("./taskController");
 
-router.get("/", taskController.getTasks).post("/task", taskController.addTask);
+router
+  .get("/", taskController.getTasks)
+  .post("/task", taskController.addTask)
+  .put("/task/:taskId", taskController.updateTask);
 
 module.exports = router;
